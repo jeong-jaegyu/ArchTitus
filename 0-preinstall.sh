b30e6c6a34bfcf152f030dc4dd0156dddf3ef067
Lin Minsu <linminsuwastaken@gmail.com>
2021-11-18 13:57:16 -0500

    yes

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -69,10 +69,10 @@ echo -e "\nCreating Filesystems...\n$HR"
 if [[ ${DISK} =~ "nvme" ]]; then
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}p2"
 mkfs.ext4 -L "ROOT" "${DISK}p3"
-mount -t ext4 "${DISK}p3" /mnt
+mount "${DISK}p3" /mnt
 else
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}2"
-mkfs.ext4 -L "ROOT" "${DISK}3
+mkfs.ext4 -L "ROOT" "${DISK}3"
 mount -t ext4 "${DISK}3" /mnt
 fi
 umount /mnt
