e7147f1c01e7f2ab83c67c10b953c06bf89b4c59
Lin Minsu <linminsuwastaken@gmail.com>
2021-11-18 13:44:49 -0500

    yes

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -99,12 +99,19 @@ if ! grep -qs '/mnt' /proc/mounts; then
     reboot now
 fi
 
-pacman -S --noconfirm gptfdisk btrfs-progs
+if [[ ! -d "/sys/firmware/efi "]]; then
+    grub-install --boot-directory=/mnt/boot ${DISK}
+fi
 pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
+read "Pacstrap"
+
 genfstab -U /mnt >> /mnt/etc/fstab
+read "genfstab"
+
 echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+read "keys"
 echo "--------------------------------------"
 echo "--GRUB BIOS Bootloader Install&Check--"
 echo "--------------------------------------"
